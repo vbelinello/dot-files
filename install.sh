08a9a1c5408d687481ed6f411665a7b3cb129d24
Victor Belinello <victor@driva.com.br>
2023-10-01 08:07:47 -0300

    basic packages

---
+++ install.sh
@@ -0,0 +1,11 @@
+# Install frontend for apt
+sudo apt install -y nala
+
+# Install basic packages
+sudo nala install -y pipewire-audio bluez git btop bat ripgrep unzip zsh
+
+# Install fonts
+sudo nala install -y fonts-noto
+
+# Install i3 and utils
+sudo nala install -y i3-wm rofi feh picom dunst
