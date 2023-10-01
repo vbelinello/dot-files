c0a4efe6882ab88bd9a48fe45bdedc289dd0e6b4
Victor Belinello <victor@driva.com.br>
2023-10-01 10:01:08 -0300

    more packages

--- install.sh
+++ install.sh
@@ -4,10 +4,11 @@ sudo apt update
 sudo apt install -y nala
 
 # Install basic packages
-sudo nala install -y curl wget unzip fonts-noto build-essential neovim btop bat ripgrep zsh jq
+sudo nala install -y curl wget unzip fonts-noto build-essential neovim btop bat ripgrep zsh jq gnupg software-properties-common
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.13.1
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
-# Install i3 and utils
+# Install i3 and desktop utils
 sudo nala install -y xorg pipewire-audio bluez i3-wm alacritty rofi feh picom dunst flameshot libreoffice-calc mpv
 
 ## Chrome
@@ -38,6 +39,6 @@ sudo nala install -y ./gh.deb
 rm gh.deb
 
 sudo chsh -s $(which zsh) victor
-rmdir ~/Desktop ~/Downloads ~/Documents ~/Public ~/Templates ~/Video ~/Pictures
+rmdir ~/Desktop ~/Downloads ~/Documents ~/Pictures ~/Public ~/Music ~/Templates ~/Videos
 
 cp -R .config wallpapers .xinitrc .XResources .zprofile ~/
