adc202415fb087edc164a3da0e7298faf2198adf
Victor Belinello <victor@driva.com.br>
2023-10-01 08:19:10 -0300

    more packages

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 sudo apt install -y nala
 
 # Install basic packages
-sudo nala install -y pipewire-audio bluez git btop bat ripgrep unzip zsh
-
-# Install fonts
-sudo nala install -y fonts-noto
+sudo nala install -y pipewire-audio bluez git btop bat ripgrep unzip zsh fonts-noto
 
 # Install i3 and utils
 sudo nala install -y i3-wm rofi feh picom dunst
+
+# Install utils
+sudo nala install -y libreoffice-calc
