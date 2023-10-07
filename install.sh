d8481a9b4217bff83cc366c334754736907faeae
Victor Belinello <victor@driva.com.br>
2023-10-07 16:49:32 -0300

    basic config

--- install.sh
+++ install.sh
@@ -1,56 +1,24 @@
-sudo apt update
+# Build
+sudo pacman -Sy git base-devel openssl zlib xz tk unzip libx11 libxft libxinerama
 
-# Install frontend for apt
-sudo apt install -y nala
+# Dev
+sudo pacman -Sy zsh btop neovim dbeaver github-cli
 
-# Install basic packages
-sudo nala install -y curl wget unzip fonts-noto rsync neovim neofetch btop bat ripgrep zsh jq gnupg software-properties-common
-# Build deps
-sudo nala install -y build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
-sudo nala install -y autoconf patch rustc libyaml-dev libreadline6-dev libgmp-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev uuid-dev
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.13.1
-echo '. "$HOME/.asdf/asdf.sh"' >> ~/.zshrc
 
-curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+curl -L https://aka.ms/InstallAzureCli | bash
 
-## Extra fonts
-mkdir -p ~/.local/share/fonts
-wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
-mv 'MesloLGS NF Regular.ttf' ~/.local/share/fonts
-fc-cache -fv ~/.local/share/fonts
+# Utils
+sudo pacman -Sy libreoffice-still pipewire wireplumber pipewire-audio pipewire-alsa pipewire-pulse bluez bluez-utils brightnessctl  wget curl unzip rsync neofetch
 
-# Install i3 and desktop utils
-sudo nala install -y xorg pipewire-audio bluez i3-wm alacritty rofi feh picom dunst flameshot libreoffice-calc mpv
-
-## Chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb  -O chrome.deb
-sudo nala install -y ./chrome.deb
-rm chrome.deb
-## Vscode
-wget https://az764295.vo.msecnd.net/stable/abd2f3db4bdb28f9e95536dfa84d8479f1eb312d/code_1.82.2-1694671812_amd64.deb  -O code.deb
-sudo nala install -y ./code.deb
-rm code.deb
-## Discord
-wget "https://discord.com/api/download?platform=linux&format=deb" -O discord.deb
-sudo nala install -y ./discord.deb
-rm discord.deb
 ## ZSH plugin manager
 zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
-## Dbeaver
-wget https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb -O dbeaver.deb
-sudo nala install -y ./dbeaver.deb
-rm dbeaver.deb
-## Insomnia
-wget https://download.konghq.com/insomnia-ubuntu/pool/all/i/insomnia/insomnia_8.1.0_amd64.deb -O insomnia.deb
-sudo nala install -y ./insomnia.deb
-rm insomnia.deb
-## Github CLI
-wget https://github.com/cli/cli/releases/download/v2.35.0/gh_2.35.0_linux_amd64.deb -O gh.deb
-sudo nala install -y ./gh.deb
-rm gh.deb
-
 sudo chsh -s $(which zsh) victor
 
-cp -R .config wallpapers .xinitrc .XResources .zprofile ~/
+git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 
-sudo cp 50-touchpad.conf /etc/X11/xorg.conf.d/
+yay -S nerd-fonts discord-ptb google-chrome insomnia-bin visual-studio-code-bin
+
+# Files
+cp -R .config wallpapers xorg/.xinitrc xorg/.XResources .zprofile ~/
+sudo cp xorg/50-touchpad.conf /etc/X11/xorg.conf.d/
--- post.sh
+++ post.sh
@@ -1,5 +1,3 @@
-rmdir ~/Desktop ~/Downloads ~/Documents ~/Pictures ~/Public ~/Music ~/Templates ~/Videos
-
 asdf update 
 asdf plugin add python
 asdf plugin add nodejs
@@ -24,6 +22,3 @@ sudo mount /dev/sda1 ~/usb
 sudo cp -R ~/usb/.ssh ~
 sudo chown -R victor: ~/.ssh
 chmod 400 ~/.ssh/*
-eval `ssh-agent` && ssh-add ~/.ssh/id_ed25519 
-
-rsync -avP --exclude='__pycache__' --exclude='.venv' --exclude='node_modules' --exclude='session' --exclude='.mypy_cache' --exclude='bronze' --exclude='dist' --exclude='pdfs/cau' --exclude='*.parquet' --exclude='.terraspace-cache' --exclude='data_acquisition' victor@192.168.1.11:/home/victor/driva /home/victor/
