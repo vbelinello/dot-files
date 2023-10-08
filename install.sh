58639d3eea110707e85f4ba508ae9e1578ac2cdc
Victor Belinello <victor@driva.com.br>
2023-10-08 07:34:28 -0300

    add packges

--- install.sh
+++ install.sh
@@ -1,15 +1,13 @@
 # Basic
-sudo pacman -Sy xorg xorg-xinit git base-devel openssl zlib xz tk unzip libx11 libxft libxinerama
+sudo pacman -Sy xorg xorg-xinit git base-devel openssl zlib xz tk unzip libx11 libxft libxinerama openssh
 
 # Dev
 sudo pacman -Sy zsh btop neovim dbeaver github-cli
 
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.13.1
 
-curl -L https://aka.ms/InstallAzureCli | bash
-
 # Utils
-sudo pacman -Sy libreoffice-still pipewire wireplumber pipewire-audio pipewire-alsa pipewire-pulse bluez bluez-utils brightnessctl  wget curl unzip rsync neofetch picom feh
+sudo pacman -Sy libreoffice-still pipewire wireplumber pipewire-audio pipewire-alsa pipewire-pulse bluez bluez-utils brightnessctl  wget curl unzip rsync neofetch picom feh jq
 
 ## ZSH plugin manager
 zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
@@ -18,7 +16,7 @@ sudo chsh -s $(which zsh) victor
 cd ~
 git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 
-yay -S nerd-fonts discord-ptb google-chrome insomnia-bin visual-studio-code-bin
+yay -S nerd-fonts discord-ptb google-chrome insomnia-bin visual-studio-code-bin azure-cli-bin
 
 # Files
 cp -R dot-files/.config dot-files/wallpapers dot-files/xorg/.xinitrc dot-files/xorg/.Xresources dot-files/.zprofile ~/
--- post.sh
+++ post.sh
@@ -4,21 +4,22 @@ asdf plugin add nodejs
 asdf plugin add ruby
 asdf plugin add kubelogin
 asdf plugin add kubectl
+asdf plugin-add terraform https://github.com/asdf-community/asdf-hashicorp.git
+
 
 asdf install nodejs latest
 asdf install ruby latest
 asdf install kubectl latest
 asdf install kubelogin latest
+asdf install terraform latest
 asdf install python 3.11.5
+asdf install python 3.10.12
 
 asdf global nodejs latest
 asdf global ruby latest
 asdf global kubectl latest
 asdf global kubelogin latest
+asdf global terraform latest
 asdf global python 3.11.5
 
-mkdir ~/usb
-sudo mount /dev/sda1 ~/usb
-sudo cp -R ~/usb/.ssh ~
-sudo chown -R victor: ~/.ssh
-chmod 400 ~/.ssh/*
+gem install terraspace
