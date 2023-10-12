5c4192b2d102c82606c2cdcc204830d27de553cf
Victor Hugo <118543058+vbelinello@users.noreply.github.com>
2023-10-12 10:52:22 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 # Basic
 sudo pacman -Syyuu
 
-sudo pacman -Sy git zsh btop neovim dbeaver github-cli jq base-devel openssl zlib xz tk unzip openssh rsync neofetch wget curl libreoffice-still spectacle
+sudo pacman -Sy git zsh btop neovim dbeaver github-cli jq base-devel openssl zlib xz tk unzip openssh rsync neofetch wget curl libreoffice-still starship spectacle
 
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.13.1
 
